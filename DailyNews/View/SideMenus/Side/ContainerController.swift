3670776785420d260915c5e27426eae83b070975
NizarNaufal <32515100+NizarNaufal@users.noreply.github.com>
2021-03-04 12:14:27 +0700

    Update News, SideMenus Header & Cells

---
+++ DailyNews/View/SideMenus/Side/ContainerController.swift
@@ -0,0 +1,76 @@
+//
+//  ContainerController.swift
+//  DailyNews
+//
+//  Created by nizar on 04/03/21.
+//
+
+import UIKit
+import TinyConstraints
+
+class ContainerController: UIViewController {
+
+    let tabBar = TabBarController()
+    var menuController: SideMenuController!
+    private var isMenuHidden = true
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupTabBarView()
+        tabBar.menuDelegate = self
+    }
+
+    func setupTabBarView() {
+        view.addSubview(tabBar.view)
+        addChild(tabBar)
+        tabBar.didMove(toParent: self)
+    }
+
+    func setupMenuController() {
+        if menuController == nil {
+            menuController = SideMenuController()
+            menuController.sourceDelegate = self
+            addChild(menuController)
+            view.insertSubview(menuController.view, at: 0)
+            menuController.didMove(toParent: self)
+            menuController.view.edgesToSuperview(excluding: .trailing)
+            menuController.view.width(self.tabBar.view.frame.width-165)
+        }
+    }
+
+    func showMenuController(shouldExpand: Bool) {
+        if !shouldExpand {
+            UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.8,
+                           initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                self.tabBar.view.frame.origin.x = self.tabBar.view.frame.width - 165
+            }, completion: { (_) in
+                self.tabBar.feedVC.view.isUserInteractionEnabled = false
+            })
+        } else {
+            UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.8,
+                           initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                self.tabBar.view.frame.origin.x = 0
+            }, completion: { (_) in
+                self.isMenuHidden = true
+                self.tabBar.feedVC.view.isUserInteractionEnabled = true
+            })
+        }
+    }
+}
+extension ContainerController: SlideMenuDelegate, SourcesViewControllerDelegate {
+
+    func pushToSourcesVC(source: UIViewController) {
+        showMenuController(shouldExpand: true)
+        source.modalPresentationStyle = .fullScreen
+        tabBar.feedVC.navigationController?.pushViewController(source, animated: true)
+    }
+
+    func configureSlideMenu() {
+        if isMenuHidden {
+            setupMenuController()
+        }
+        isMenuHidden = !isMenuHidden
+        showMenuController(shouldExpand: isMenuHidden)
+    }
+}
---
+++ DailyNews/View/SideMenus/Side/TabBarController.swift
@@ -0,0 +1,54 @@
+//
+//  TabBarController.swift
+//  DailyNews
+//
+//  Created by nizar on 04/03/21.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    var feedVC = NewsViewController()
+    let searchVC = SearchNewsController()
+    let categoriesVC = CategoriesViewController()
+    weak var menuDelegate: SlideMenuDelegate?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureTabBar()
+        setupMenuButton()
+
+    }
+
+    func configureTabBar() {
+        feedVC.title = "News"
+        feedVC.tabBarItem = UITabBarItem(title: "News", image: UIImage(systemName: "house"), tag: 0)
+        categoriesVC.title = "Categories"
+        categoriesVC.tabBarItem = UITabBarItem(title: "Categories", image: UIImage(systemName: "paperplane"), tag: 1)
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 2)
+
+        let feedNavigationController = UINavigationController(rootViewController: feedVC)
+        let categoriesNavigationController = UINavigationController(rootViewController: categoriesVC)
+        let searchNavigationController = UINavigationController(rootViewController: searchVC)
+        searchNavigationController.navigationBar.prefersLargeTitles = true
+        viewControllers = [feedNavigationController, categoriesNavigationController, searchNavigationController]
+
+    }
+
+    private func setupMenuButton() {
+        let menuButton = UIBarButtonItem(image: UIImage(named: "hamburger.png"),
+                                         style: .plain, target: self, action: #selector(openMenu))
+        feedVC.navigationItem.leftBarButtonItem = menuButton
+        feedVC.navigationItem.leftBarButtonItem?.tintColor = .black
+    }
+
+    @objc func openMenu() {
+        menuDelegate?.configureSlideMenu()
+    }
+
+
+}
